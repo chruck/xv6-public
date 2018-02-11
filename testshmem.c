71af2ecf35b83415773549be47c9eda25fb03704
Jas Eckard <eckard@clemson.edu>
2018-02-11 15:34:58 -0500

    Fleshed out:  3 children, all writing to same page

--- testshmem.c
+++ testshmem.c
@@ -11,7 +11,83 @@
 * Testing Project 2 Part B:  shmem system calls
 */
 
+//#include "types.h"  // uint
+//#include "user.h"   // printf() and fork()
+//#include "defs.h"   // getproc(), strncpy()
+
+#include "param.h"
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
+#include "fcntl.h"
+#include "syscall.h"
+#include "traps.h"
+#include "memlayout.h"
+
+
+#define NULL (void *)(0)
+#define stdout 1
+#define out(...) printf(stdout, __VA_ARGS__)
+
 int main(int argc, char *argv[])
 {
+        int pid = fork();
+        void *pg_addr = NULL;
+
+        if (0 == pid) {  // child 1
+                pid = getpid();
+                //pg_addr = shmem_access(0);
+                //strcpy((char *)pg_addr, "foo\0");
+
+                out("child1[%d]: %d processes accessing shared pg 0 (%s)\n",
+                    pid, shmem_count(0), (char *)pg_addr);
+
+                exit();
+        }
+
+        if (pid < 0) {
+                out("fork() failed creating child 1\n");
+                exit();
+        }
+
+        if (0 == (pid = fork())) {  // child 2
+                pid = getpid();
+                //pg_addr = shmem_access(0);
+                //strcpy((char *)pg_addr, "bar\0");
+
+                out("child2[%d]: %d processes accessing shared pg 0 (%s)\n",
+                    pid, shmem_count(0), (char *)pg_addr);
+
+                exit();
+        }
+
+        if (pid < 0) {
+                out("fork() failed creating child 2\n");
+                exit();
+        }
+
+        if (0 == (pid = fork())) {  // child 3
+                pid = getpid();
+                //pg_addr = shmem_access(0);
+                //strcpy((char *)pg_addr, "baz\0");
+
+                out("child3[%d]: %d processes accessing shared pg 0 (%s)\n",
+                    pid, shmem_count(0), (char *)pg_addr);
+
+                exit();
+        }
+
+        if (pid < 0) {
+                out("fork() failed creating child 3\n");
+                exit();
+        }
+
+        //pg_addr = shmem_access(0);
+        //strcpy((char *)pg_addr, "qux\0");
+
+        out("parent[%d]: %d processes accessing shared pg 0 (%s)\n",
+            pid, shmem_count(0), (char *)pg_addr);
+
         return 0;
 }
