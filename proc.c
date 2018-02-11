ff900f4ee14591755b0f343d4367b4c774ab3f36
Jas Eckard <eckard@clemson.edu>
2018-02-10 23:52:41 -0500

    jasindent'd

--- proc.c
+++ proc.c
@@ -180,45 +180,44 @@ growproc(int n)
 int
 fork(void)
 {
-  int i, pid;
-  struct proc *np;
-  struct proc *curproc = myproc();
+        int i, pid;
+        struct proc *np;
+        struct proc *curproc = myproc();
 
-  // Allocate process.
-  if((np = allocproc()) == 0){
-    return -1;
-  }
+        // Allocate process.
+        if ((np = allocproc()) == 0) {
+                return -1;
+        }
+        // Copy process state from proc.
+        if ((np->pgdir = copyuvm(curproc->pgdir, curproc->sz)) == 0) {
+                kfree(np->kstack);
+                np->kstack = 0;
+                np->state = UNUSED;
+                return -1;
+        }
+        np->sz = curproc->sz;
+        np->parent = curproc;
+        *np->tf = *curproc->tf;
 
-  // Copy process state from proc.
-  if((np->pgdir = copyuvm(curproc->pgdir, curproc->sz)) == 0){
-    kfree(np->kstack);
-    np->kstack = 0;
-    np->state = UNUSED;
-    return -1;
-  }
-  np->sz = curproc->sz;
-  np->parent = curproc;
-  *np->tf = *curproc->tf;
+        // Clear %eax so that fork returns 0 in the child.
+        np->tf->eax = 0;
 
-  // Clear %eax so that fork returns 0 in the child.
-  np->tf->eax = 0;
+        for (i = 0; i < NOFILE; i++)
+                if (curproc->ofile[i])
+                        np->ofile[i] = filedup(curproc->ofile[i]);
+        np->cwd = idup(curproc->cwd);
 
-  for(i = 0; i < NOFILE; i++)
-    if(curproc->ofile[i])
-      np->ofile[i] = filedup(curproc->ofile[i]);
-  np->cwd = idup(curproc->cwd);
+        safestrcpy(np->name, curproc->name, sizeof(curproc->name));
 
-  safestrcpy(np->name, curproc->name, sizeof(curproc->name));
+        pid = np->pid;
 
-  pid = np->pid;
+        acquire(&ptable.lock);
 
-  acquire(&ptable.lock);
+        np->state = RUNNABLE;
 
-  np->state = RUNNABLE;
+        release(&ptable.lock);
 
-  release(&ptable.lock);
-
-  return pid;
+        return pid;
 }
 
 // Exit the current process.  Does not return.
