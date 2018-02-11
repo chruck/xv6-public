899f4889201f0455418577c68153d38acfb6eb44
Jas Eckard <eckard@clemson.edu>
2018-02-10 23:55:54 -0500

    Boilerplate for program to test shmem_* syscalls

---
+++ testshmem.c
@@ -0,0 +1,17 @@
+/**
+* @file testshmem.c
+* @author Jas Eckard <eckard@clemson.edu>
+*
+* @section LICENSE
+*
+* GPL license
+*
+* @section DESCRIPTION
+*
+* Testing Project 2 Part B:  shmem system calls
+*/
+
+int main(int argc, char *argv[])
+{
+        return 0;
+}
