9c7300dd9b9f23422c76e38ed96652f6ae5e734f
Jas Eckard <eckard@clemson.edu>
2018-02-12 22:15:45 -0500

    Actually has code:  fork/exec 2 test progs

--- proj2tests.c
+++ proj2tests.c
@@ -11,3 +11,41 @@
 * This file tests Project 2 modifications to xv6 source code.
 */
 
+#include "types.h"  // uint
+#include "user.h"   // fork(), exec()
+
+#define NULL (void *)(0)
+#define stdout 1
+#define out(...) printf(stdout, __VA_ARGS__)
+
+int main(int argc, char **argv)
+{
+        int rc = 0;
+        int pid = fork();
+
+        if (0 == pid) {  // child 1
+                out("Running testderefnull:\n\n");
+                rc = exec("testderefnull", NULL);
+
+                if (!rc) {
+                        out("\n... testderefnull failed.\n");
+                }
+                exit();
+        }
+
+        wait();
+
+        if (0 == pid) {  // child 2
+                out("Running testshmem:\n\n");
+                rc = exec("testshmem", NULL);
+
+                if (!rc) {
+                        out("\n... testshmem failed.\n");
+                }
+                exit();
+        }
+
+        wait();
+
+        return rc;
+}
