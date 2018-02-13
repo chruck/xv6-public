8390d35edd8a88ff8a8f960a5137112921f35f78
Jas Eckard <eckard@clemson.edu>
2018-02-12 23:44:38 -0500

    Jasindent'd: *.c and *.h

--- mmu.h
+++ mmu.h
@@ -40,11 +40,11 @@
 #define CR4_PSE         0x00000010      // Page size extension
 
 // various segment selectors.
-#define SEG_KCODE 1  // kernel code
-#define SEG_KDATA 2  // kernel data+stack
-#define SEG_UCODE 3  // user code
-#define SEG_UDATA 4  // user data+stack
-#define SEG_TSS   5  // this process's task state
+#define SEG_KCODE 1             // kernel code
+#define SEG_KDATA 2             // kernel data+stack
+#define SEG_UCODE 3             // user code
+#define SEG_UDATA 4             // user data+stack
+#define SEG_TSS   5             // this process's task state
 
 // cpu->gdt[NSEGS] holds the above segments.
 #define NSEGS     6
@@ -52,19 +52,19 @@
 #ifndef __ASSEMBLER__
 // Segment Descriptor
 struct segdesc {
-  uint lim_15_0 : 16;  // Low bits of segment limit
-  uint base_15_0 : 16; // Low bits of segment base address
-  uint base_23_16 : 8; // Middle bits of segment base address
-  uint type : 4;       // Segment type (see STS_ constants)
-  uint s : 1;          // 0 = system, 1 = application
-  uint dpl : 2;        // Descriptor Privilege Level
-  uint p : 1;          // Present
-  uint lim_19_16 : 4;  // High bits of segment limit
-  uint avl : 1;        // Unused (available for software use)
-  uint rsv1 : 1;       // Reserved
-  uint db : 1;         // 0 = 16-bit segment, 1 = 32-bit segment
-  uint g : 1;          // Granularity: limit scaled by 4K when set
-  uint base_31_24 : 8; // High bits of segment base address
+        uint lim_15_0:16;       // Low bits of segment limit
+        uint base_15_0:16;      // Low bits of segment base address
+        uint base_23_16:8;      // Middle bits of segment base address
+        uint type:4;            // Segment type (see STS_ constants)
+        uint s:1;               // 0 = system, 1 = application
+        uint dpl:2;             // Descriptor Privilege Level
+        uint p:1;               // Present
+        uint lim_19_16:4;       // High bits of segment limit
+        uint avl:1;             // Unused (available for software use)
+        uint rsv1:1;            // Reserved
+        uint db:1;              // 0 = 16-bit segment, 1 = 32-bit segment
+        uint g:1;               // Granularity: limit scaled by 4K when set
+        uint base_31_24:8;      // High bits of segment base address
 };
 
 // Normal segment
@@ -78,29 +78,29 @@ struct segdesc {
   (uint)(lim) >> 16, 0, 0, 1, 0, (uint)(base) >> 24 }
 #endif
 
-#define DPL_USER    0x3     // User DPL
+#define DPL_USER    0x3         // User DPL
 
 // Application segment type bits
-#define STA_X       0x8     // Executable segment
-#define STA_E       0x4     // Expand down (non-executable segments)
-#define STA_C       0x4     // Conforming code segment (executable only)
-#define STA_W       0x2     // Writeable (non-executable segments)
-#define STA_R       0x2     // Readable (executable segments)
-#define STA_A       0x1     // Accessed
+#define STA_X       0x8         // Executable segment
+#define STA_E       0x4         // Expand down (non-executable segments)
+#define STA_C       0x4         // Conforming code segment (executable only)
+#define STA_W       0x2         // Writeable (non-executable segments)
+#define STA_R       0x2         // Readable (executable segments)
+#define STA_A       0x1         // Accessed
 
 // System segment type bits
-#define STS_T16A    0x1     // Available 16-bit TSS
-#define STS_LDT     0x2     // Local Descriptor Table
-#define STS_T16B    0x3     // Busy 16-bit TSS
-#define STS_CG16    0x4     // 16-bit Call Gate
-#define STS_TG      0x5     // Task Gate / Coum Transmitions
-#define STS_IG16    0x6     // 16-bit Interrupt Gate
-#define STS_TG16    0x7     // 16-bit Trap Gate
-#define STS_T32A    0x9     // Available 32-bit TSS
-#define STS_T32B    0xB     // Busy 32-bit TSS
-#define STS_CG32    0xC     // 32-bit Call Gate
-#define STS_IG32    0xE     // 32-bit Interrupt Gate
-#define STS_TG32    0xF     // 32-bit Trap Gate
+#define STS_T16A    0x1         // Available 16-bit TSS
+#define STS_LDT     0x2         // Local Descriptor Table
+#define STS_T16B    0x3         // Busy 16-bit TSS
+#define STS_CG16    0x4         // 16-bit Call Gate
+#define STS_TG      0x5         // Task Gate / Coum Transmitions
+#define STS_IG16    0x6         // 16-bit Interrupt Gate
+#define STS_TG16    0x7         // 16-bit Trap Gate
+#define STS_T32A    0x9         // Available 32-bit TSS
+#define STS_T32B    0xB         // Busy 32-bit TSS
+#define STS_CG32    0xC         // 32-bit Call Gate
+#define STS_IG32    0xE         // 32-bit Interrupt Gate
+#define STS_TG32    0xF         // 32-bit Trap Gate
 
 // A virtual address 'la' has a three-part structure as follows:
 //
@@ -151,57 +151,57 @@ typedef uint pte_t;
 
 // Task state segment format
 struct taskstate {
-  uint link;         // Old ts selector
-  uint esp0;         // Stack pointers and segment selectors
-  ushort ss0;        //   after an increase in privilege level
-  ushort padding1;
-  uint *esp1;
-  ushort ss1;
-  ushort padding2;
-  uint *esp2;
-  ushort ss2;
-  ushort padding3;
-  void *cr3;         // Page directory base
-  uint *eip;         // Saved state from last task switch
-  uint eflags;
-  uint eax;          // More saved state (registers)
-  uint ecx;
-  uint edx;
-  uint ebx;
-  uint *esp;
-  uint *ebp;
-  uint esi;
-  uint edi;
-  ushort es;         // Even more saved state (segment selectors)
-  ushort padding4;
-  ushort cs;
-  ushort padding5;
-  ushort ss;
-  ushort padding6;
-  ushort ds;
-  ushort padding7;
-  ushort fs;
-  ushort padding8;
-  ushort gs;
-  ushort padding9;
-  ushort ldt;
-  ushort padding10;
-  ushort t;          // Trap on task switch
-  ushort iomb;       // I/O map base address
+        uint link;              // Old ts selector
+        uint esp0;              // Stack pointers and segment selectors
+        ushort ss0;             //   after an increase in privilege level
+        ushort padding1;
+        uint *esp1;
+        ushort ss1;
+        ushort padding2;
+        uint *esp2;
+        ushort ss2;
+        ushort padding3;
+        void *cr3;              // Page directory base
+        uint *eip;              // Saved state from last task switch
+        uint eflags;
+        uint eax;               // More saved state (registers)
+        uint ecx;
+        uint edx;
+        uint ebx;
+        uint *esp;
+        uint *ebp;
+        uint esi;
+        uint edi;
+        ushort es;              // Even more saved state (segment selectors)
+        ushort padding4;
+        ushort cs;
+        ushort padding5;
+        ushort ss;
+        ushort padding6;
+        ushort ds;
+        ushort padding7;
+        ushort fs;
+        ushort padding8;
+        ushort gs;
+        ushort padding9;
+        ushort ldt;
+        ushort padding10;
+        ushort t;               // Trap on task switch
+        ushort iomb;            // I/O map base address
 };
 
 // PAGEBREAK: 12
 // Gate descriptors for interrupts and traps
 struct gatedesc {
-  uint off_15_0 : 16;   // low 16 bits of offset in segment
-  uint cs : 16;         // code segment selector
-  uint args : 5;        // # args, 0 for interrupt/trap gates
-  uint rsv1 : 3;        // reserved(should be zero I guess)
-  uint type : 4;        // type(STS_{TG,IG32,TG32})
-  uint s : 1;           // must be 0 (system)
-  uint dpl : 2;         // descriptor(meaning new) privilege level
-  uint p : 1;           // Present
-  uint off_31_16 : 16;  // high bits of offset in segment
+        uint off_15_0:16;       // low 16 bits of offset in segment
+        uint cs:16;             // code segment selector
+        uint args:5;            // # args, 0 for interrupt/trap gates
+        uint rsv1:3;            // reserved(should be zero I guess)
+        uint type:4;            // type(STS_{TG,IG32,TG32})
+        uint s:1;               // must be 0 (system)
+        uint dpl:2;             // descriptor(meaning new) privilege level
+        uint p:1;               // Present
+        uint off_31_16:16;      // high bits of offset in segment
 };
 
 // Set up a normal interrupt/trap gate descriptor.
