e5a26e095748a55a510e82000617acf197e56012
Jas Eckard <eckard@clemson.edu>
2018-02-15 00:03:04 -0500

    Created systhread.c for clone() and join() thread syscalls

---
+++ systhread.c
@@ -0,0 +1,25 @@
+/**
+* @file systhread.c
+* @author Jas Eckard <eckard@clemson.edu>
+*
+* @section LICENSE
+*
+* GPLv3
+*
+* @section DESCRIPTION
+*
+* This file implements threading in the xv6 kernel
+*/
+
+int sys_clone(void)
+{
+        //return clone();
+        return 0;
+}
+
+int sys_join(void)
+{
+        //return join();
+        return 0;
+}
+
