45709ee6838494b50c3ff9e641c2b21c00e14d3b
Jas Eckard <eckard@clemson.edu>
2018-02-26 00:13:40 -0500

    Changed from thread_num to thread_id

--- proc.h
+++ proc.h
@@ -51,7 +51,7 @@ struct proc {
         struct inode *cwd;           // Current directory
         char name[16];               // Process name (debugging)
 // TODO:  Document, adjust fn's that use `struct proc`:
-        uint thread_num;             // Thread number
+        uint thread_id;              // Thread ID
 };
 
 // Process memory is laid out contiguously, low addresses first:
