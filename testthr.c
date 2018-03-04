725ccd9d6d209d23e1c0ba6d6cbf93c8481ad6fb
Jas Eckard <eckard@clemson.edu>
2018-03-03 20:04:59 -0500

    Create testnulllock(), testnullthread(); returns uniq; err strs returned; output

--- testthr.c
+++ testthr.c
@@ -1,48 +1,86 @@
-#include "types.h"
-#include "stat.h"
-#include "user.h"
-#include "kthreads.h"
+#include "types.h"  // uint
+//#include "stat.h"
+#include "user.h"  // strcpy, syscalls
+#include "kthreads.h"  // lock_t, kthread
 
-//#define LOCKS_ON 1
-int LOCKS_ON = 1;
-#define NULL (void *)0
 #define stdout 1
 #define out(...) printf(stdout, __VA_ARGS__)
-
 
 // global lock for product
 lock_t lock_g;
 
-#define NUM_PROD 3
-#define NUM_CONS 2
-#define TOTAL_PRODUCTS 10000000
-
-int testgloballock()
+int testnulllock(char *err)
 {
-        if (!lock_g) {
-                out("Global lock doesn't exist.\n");
+        int rc = 0;
+
+        if (-1 != (rc = init_lock(NULL))) {
+                strcpy(err, "err:  init_lock() accepts NULL for lock.\n");
                 return -1;
         }
 
-        if (!init_lock(&lock_g)) {
-                out("Unable to init global lock.\n");
+        if (-1 != (rc = lock_acquire(NULL))) {
+                strcpy(err, "err:  lock_acquire() accepts NULL for lock.\n");
                 return -2;
+        }
+
+        if (-1 != (rc = lock_release(NULL))) {
+                strcpy(err, "err:  lock_release() accepts NULL for lock.\n");
+                return -3;
         }
 
         return 0;
 }
 
-int testthread_create()
+int testgloballock(char *err)
+{
+        int rc = 0;
+
+        if (0 != lock_g) {
+                strcpy(err, "err:  Global lock set to locked.\n");
+                return -4;
+        }
+
+        if ((rc = init_lock(&lock_g))) {
+                strcpy(err, "err:  Unable to init global lock.\n");
+                return -5;
+        }
+
+        return 0;
+}
+
+int testnullthread(char *err)
+{
+        int rc = 0;
+
+        //if (-1 != (rc = thread_create(NULL, NULL))) {
+        //struct kthread thr = thread_create(NULL, NULL);
+        /*
+        if (-1 != (thread_create(NULL, NULL))) {
+                strcpy(err, "err:  thread_create() accepts NULL for start_routine.  \n", rc);
+                return -6;
+        }
+        */
+
+        struct kthread emptythr = {0};
+        if (-1 != (rc = thread_join(emptythr))) {
+                strcpy(err, "err:  thread_join() accepts NULL for struct kthread.\n");
+                return -7;
+        }
+
+        return 0;
+}
+
+int testthread_create(char *err)
 {
         int rc = 0;
         kthread_t thr = thread_create(NULL, NULL);
 
         if (0 == thr.pid) {
-                out("Empty thread_create() returned PID 0, weird\n");
+                strcpy(err, "Empty thread_create() returned PID 0, weird\n");
         } else if (0 > thr.pid) {
-                out("Empty thread_create() returned PID %d, as expected\n", rc);
+                strcpy(err, "Empty thread_create() returned PID, as expected\n");
         } else {
-                out("Empty thread_create() error:  returned PID %d\n", rc);
+                strcpy(err, "Empty thread_create() error:  returned PID\n");
         }
 
         return !rc;
@@ -50,6 +88,30 @@ int testthread_create()
 
 int main(void)
 {
-        return testgloballock()
-                && testthread_create();
+        int rc = 0;
+        /*
+        char err[255] = "no error";
+
+        if ((rc = testnulllock(err))) {
+                out("testnulllock() failed, rc=%d:\n\t%s", rc, err);
+                return rc;
+        }
+
+        if ((rc = testgloballock(err))) {
+                out("testgloballock() failed, rc=%d:\n\t%s", rc, err);
+                return rc;
+        }
+
+        if ((rc = testnullthread(err))) {
+                out("testnullthread() failed, rc=%d:\n\t%s", rc, err);
+                return rc;
+        }
+
+        if ((rc = testthread_create(err))) {
+                out("testthread_create() failed, rc=%d:\n\t%s", rc, err);
+                return rc;
+        }
+        */
+
+        return rc;
 }
