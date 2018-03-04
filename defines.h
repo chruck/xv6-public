363b93819082e525d9c9a560b73cec967fb094fe
Jas Eckard <eckard@clemson.edu>
2018-03-03 21:43:03 -0500

    Decided to create defines.h for NULL, TRUE, SUCCESS, etc

---
+++ defines.h
@@ -0,0 +1,13 @@
+// Seemingly standard #defines
+
+#define NULL ((void *)0)
+
+#define TRUE  1
+#define FALSE 0
+
+// Return codes
+#define SUCCESS  0
+#define RC_ERR  -1
+
+// streams
+#define stdout 1
