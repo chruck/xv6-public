5dd239f0eb1c554028b2c2d80a892c8f741cad82
Jas Eckard <eckard@clemson.edu>
2018-03-06 01:01:56 -0500

    Added #define guards; GPLv3

--- defines.h
+++ defines.h
@@ -4,12 +4,15 @@
 *
 * @section LICENSE
 *
-* (GPL license)
+* GPLv3
 *
 * @section DESCRIPTION
 *
 * This file has seemingly standard #define's.
 */
+
+#ifndef defines_h
+#define defines_h
 
 #define NULL ((void *)0)
 
@@ -22,3 +25,5 @@
 
 // streams
 #define stdout 1
+
+#endif  // defines_h
