1820b795fe76441127bf043463c35d49f0d9bbcd
Jas Eckard <eckard@clemson.edu>
2018-04-07 18:25:06 -0400

    From project3, copied defines.h

---
+++ defines.h
@@ -0,0 +1,29 @@
+/**
+* @file defines.h
+* @author Jas Eckard <eckard@clemson.edu>
+*
+* @section LICENSE
+*
+* GPLv3
+*
+* @section DESCRIPTION
+*
+* This file has seemingly standard #define's.
+*/
+
+#ifndef defines_h
+#define defines_h
+
+#define NULL ((void *)0)
+
+#define TRUE  1
+#define FALSE 0
+
+// Return codes
+#define SUCCESS  0
+#define RC_ERR  -1
+
+// streams
+#define stdout 1
+
+#endif  // defines_h
